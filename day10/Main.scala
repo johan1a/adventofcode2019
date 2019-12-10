3108ca4ce6e2a4a9a58165fc4484fb61801cf54c
Johan Andersson <joha@netcompany.com>
2019-12-10 20:34:37 +0100

    day10 use angles

--- day10/Main.scala
+++ day10/Main.scala
@@ -5,6 +5,8 @@ case class Asteroid(x: Double, y: Double)
 
 object Main extends App {
 
+  println(makeAngle((0,-1)))
+
   assert(solve("test1.txt") == Asteroid(5,8)) // 33
   assert(solve("test2.txt") == Asteroid(1,2)) // 35
   assert(solve("test3.txt") == Asteroid(6,3)) // 41
@@ -24,14 +26,25 @@ object Main extends App {
   def countVisible(asteroids: List[Asteroid], src: Asteroid): Int = {
     val sorted = asteroids.filter { a => a != src }.sortWith { (a, b) => dist(src, a) < dist(src, b) }.toList
     assert(sorted.size == asteroids.size - 1)
-    sorted.map { a => makeVector(src, a) }.toSet.size
+    sorted.map { a => makeAngle(makeVector(src, a)) }.toSet.size
   }
 
   // Lines are represented by the k in y = kx + m
   // with a in origo
   def makeVector(a: Asteroid, b: Asteroid): (Double, Double) = {
     val length = dist(a, b)
+    //println(makeAngle(precision((b.y - a.y) / length), precision((b.x - a.x) / length)))
+
     (precision((b.y - a.y) / length), precision((b.x - a.x) / length))
+  }
+
+  def makeAngle(xy: (Double, Double)): Double = {
+    val angle = Math.atan2(xy._2,  xy._1)
+    if(angle < 0) {
+      Math.PI - angle
+    } else {
+      angle
+    }
   }
 
   def precision(k: Double, digits: Int = 13): Double = {
