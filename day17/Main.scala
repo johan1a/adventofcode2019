c9526cb48008701bfec2d5501223736b8771fc54
Johan Andersson <joha@netcompany.com>
2019-12-25 13:55:28 +0100

    day17 part 1

---
+++ day17/Main.scala
@@ -0,0 +1,305 @@
+import scala.collection.mutable
+import scala.io.Source
+import scala.util.Random
+import io.AnsiColor._
+
+object Main extends App {
+
+  val ADD = 1
+  val MUL = 2
+  val INPUT = 3
+  val OUTPUT = 4
+  val JNZ = 5
+  val JEZ = 6
+  val LESS_THAN = 7
+  val EQUALS = 8
+  val RELATIVE_BASE = 9
+  val EXIT = 99
+
+  val HIT_WALL = 0
+  val MOVED = 1
+  val FOUND_OXYGEN = 2
+
+  val WALL = 35
+  val EMPTY = 46
+  val NEW_LINE = 10
+
+  val NORTH = 1
+  val SOUTH = 2
+  val WEST = 3
+  val EAST = 4
+
+  case class ComputerState(instructions: mutable.Map[BigInt, BigInt],
+                           var sp: BigInt = 0,
+                           var inputs: List[BigInt] = List(),
+                           var outputs: List[BigInt] = List(),
+                           var relativeBase: BigInt = 0,
+                           var halted: Boolean = false)
+
+  case class Pos(x: Int, y: Int)
+
+  val part1Result = part1("input.txt")
+  println(s"Part 1: ${part1Result}")
+
+  def part1(file: String): Int = {
+    var state = ComputerState(readFile(file))
+    state = runProgram(state)
+    val (map, maxX, maxY) = parse(state.outputs.map( _.toInt))
+    draw(map, maxX, maxY)
+    parameterSum(map, maxX, maxY)
+  }
+
+  type Map = mutable.Map[Pos, String]
+
+  def parameterSum(map: Map, maxX: Int, maxY: Int): Int = {
+    0.until(maxY).map { y =>
+      0.until(maxX).map { x =>
+        val pos = Pos(x, y)
+        map(pos) match {
+          case "#" => {
+            if(List(northOf(pos), southOf(pos), westOf(pos), eastOf(pos))
+              .filter { (p: Pos) => map.contains(p) && map(p) == "#" }.size == 4) {
+              x * y
+            } else {
+              0
+            }
+          }
+          case _ => 0
+        }
+      }.sum
+    }.sum
+  }
+
+  def draw(map: mutable.Map[Pos, String], maxX: Int, maxY: Int): Unit = {
+    0.until(maxY).foreach { y =>
+      0.until(maxX).foreach { x =>
+        print(prettify(map(Pos(x, y))))
+      }
+      println()
+    }
+  }
+
+  def prettify(k: String): String = {
+    k match {
+      case "#" => s"$WHITE█$RESET"
+      case "." => s"$BLACK░$RESET"
+      case "^" => "⬆"
+      case "v" => "⬇"
+      case "<" => "⬅"
+      case ">" => "➡"
+    }
+  }
+
+  def parse(outputs: List[Int]): (mutable.Map[Pos, String], Int, Int) = {
+    val map = mutable.Map[Pos, String]()
+    var maxX = -1
+    var x = 0
+    var y = 0
+    outputs.foreach { k =>
+      k match {
+        case NEW_LINE => {
+          maxX = Math.max(maxX, x)
+          x = 0
+          y += 1
+        }
+        case n => {
+          map(Pos(x, y)) = n.toChar.toString
+          x += 1
+        }
+      }
+    }
+    (map, maxX, y - 1)
+  }
+
+  def northOf(pos: Pos): Pos = {
+    Pos(pos.x, pos.y + 1)
+  }
+
+  def southOf(pos: Pos): Pos = {
+    Pos(pos.x, pos.y - 1)
+  }
+
+  def eastOf(pos: Pos): Pos = {
+    Pos(pos.x + 1, pos.y)
+  }
+
+  def westOf(pos: Pos): Pos = {
+    Pos(pos.x - 1, pos.y)
+  }
+
+  def readFile(filename: String): mutable.Map[BigInt, BigInt] = {
+    val m = mutable.Map[BigInt, BigInt]().withDefaultValue(BigInt(0))
+    val file = Source.fromFile(filename)
+    file.getLines.foreach { line =>
+      line.split(",").zipWithIndex.foreach { (intAndIndex: (String, Int)) =>
+        m(intAndIndex._2) = BigInt(intAndIndex._1.toLong)
+      }
+    }
+    file.close()
+    m
+  }
+
+  def add(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), relativeBase)
+    val b = getVal(state.instructions, state.sp + 2, paramModes(1), relativeBase)
+    setVal(state.instructions, state.sp + 3, paramModes(2), relativeBase, a + b)
+    state.sp += nbrSteps(ADD)
+  }
+
+  def mul(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), relativeBase)
+    val b = getVal(state.instructions, state.sp + 2, paramModes(1), relativeBase)
+    setVal(state.instructions, state.sp + 3, paramModes(2), relativeBase, a * b)
+    state.sp += nbrSteps(MUL)
+  }
+
+  def input(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = state.inputs.head
+    state.inputs = state.inputs.tail
+    setVal(state.instructions, state.sp + 1, paramModes.head, relativeBase, a)
+    state.sp += nbrSteps(INPUT)
+  }
+
+  def output(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), relativeBase)
+    state.sp += nbrSteps(OUTPUT)
+    state.outputs = state.outputs :+ a
+  }
+
+  def jnz(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), relativeBase)
+    val b = getVal(state.instructions, state.sp + 2, paramModes(1), relativeBase)
+    if (a != 0) {
+      state.sp = b
+    } else {
+      state.sp += nbrSteps(JNZ)
+    }
+  }
+
+  def jez(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), relativeBase)
+    val b = getVal(state.instructions, state.sp + 2, paramModes(1), relativeBase)
+    if (a == 0) {
+      state.sp = b
+    } else {
+      state.sp += nbrSteps(JEZ)
+    }
+  }
+
+  def lessThan(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), relativeBase)
+    val b = getVal(state.instructions, state.sp + 2, paramModes(1), relativeBase)
+    if (a < b) {
+      setVal(state.instructions, state.sp + 3, paramModes(2), relativeBase, 1)
+    } else {
+      setVal(state.instructions, state.sp + 3, paramModes(2), relativeBase, 0)
+    }
+    state.sp += nbrSteps(LESS_THAN)
+  }
+
+  def equal(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), relativeBase)
+    val b = getVal(state.instructions, state.sp + 2, paramModes(1), relativeBase)
+    if (a == b) {
+      setVal(state.instructions, state.sp + 3, paramModes(2), relativeBase, 1)
+    } else {
+      setVal(state.instructions, state.sp + 3, paramModes(2), relativeBase, 0)
+    }
+    state.sp += nbrSteps(EQUALS)
+  }
+
+  def setRelativeBase(state: ComputerState, paramModes: List[BigInt], relativeBase: BigInt): Unit = {
+    val a = getVal(state.instructions, state.sp + 1, paramModes(0), state.relativeBase)
+    state.relativeBase += a
+    state.sp += nbrSteps(RELATIVE_BASE)
+  }
+
+
+  def runProgram(state: ComputerState): ComputerState = {
+    val program = state.instructions
+    var opcode: Int = -1
+    var suspended = false
+    while (opcode != EXIT && !suspended) {
+      opcode = getOpcode(program(state.sp))
+      val paramModes: List[BigInt] = getParamModes(opcode, program(state.sp))
+      opcode match {
+        case ADD => add(state, paramModes, state.relativeBase)
+        case MUL => mul(state, paramModes, state.relativeBase)
+        case INPUT => {
+          if (state.inputs.isEmpty) {
+            suspended = true
+          } else {
+            input(state, paramModes, state.relativeBase)
+          }
+        }
+        case OUTPUT => output(state, paramModes, state.relativeBase)
+        case JNZ => jnz(state, paramModes, state.relativeBase)
+        case JEZ => jez(state, paramModes, state.relativeBase)
+        case LESS_THAN => lessThan(state, paramModes, state.relativeBase)
+        case EQUALS => equal(state, paramModes, state.relativeBase)
+        case RELATIVE_BASE => setRelativeBase(state, paramModes, state.relativeBase)
+        case EXIT => state.halted = true
+        case _ => {
+          println("Error: Unsupported opcode: " + opcode)
+          System.exit(1)
+        }
+      }
+    }
+    state
+  }
+
+  def getVal(program: mutable.Map[BigInt, BigInt], i: BigInt, mode: BigInt, relativeBase: BigInt): BigInt = {
+    var address = BigInt(-1)
+    if (mode == 0) {
+      address = program(i)
+    } else if (mode == 2) {
+      address = program(i) + relativeBase
+    } else {
+      address = i
+    }
+    program(address)
+  }
+
+  def setVal(program: mutable.Map[BigInt, BigInt], i: BigInt, mode: BigInt, relativeBase: BigInt, value: BigInt): Unit = {
+    var address = BigInt(-1)
+    if (mode == 0) {
+      address = program(i)
+    } else if (mode == 2) {
+      address = program(i) + relativeBase
+    } else if (mode == 1) {
+      throw new Exception("invalid mode")
+    }
+    program(address) = value
+  }
+
+  def getParamModes(opcode: BigInt, instruction: BigInt): List[BigInt] = {
+    var digits = instruction / 100 // remove opcode
+    var paramsLeft: Int = nbrSteps(opcode).toInt - 1
+    var modes = List[BigInt]()
+    while (digits > 0) {
+      modes = modes :+ (digits % 10)
+      digits /= 10
+      paramsLeft -= 1
+    }
+    0.until(paramsLeft).foreach { i =>
+      modes = modes :+ BigInt(0)
+    }
+    modes
+  }
+
+  def nbrSteps(opcode: BigInt): BigInt = {
+    opcode.intValue match {
+      case 3 => BigInt(2)
+      case 4 => BigInt(2)
+      case 5 => BigInt(3)
+      case 6 => BigInt(3)
+      case 9 => BigInt(2)
+      case _ => BigInt(4)
+    }
+  }
+
+  def getOpcode(x: BigInt): Int = {
+    (x % 100).toInt
+  }
+
+}
