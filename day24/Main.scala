c28a6ca9d86efcb21636de81e6a4f78fc2d9f93b
Johan Andersson <joha@netcompany.com>
2020-01-08 21:50:29 +0100

    day24 part 2 wip

--- day24/Main.scala
+++ day24/Main.scala
@@ -9,7 +9,7 @@ object Main extends App {
   val width = 5
 
   type Layout = Array[Array[Char]]
-  type Levels = mutable.Map[Int, Array[Array[Char]]]
+  type Levels = mutable.Map[Int, Layout]
 
   assert(calculateRating(readFile("test1.txt")) == 2129920)
 
@@ -29,65 +29,227 @@ object Main extends App {
   println(s"Part1: $part1Result")
 
   def part2(file: String): BigInt = {
-    var levels: Levels = mutable.Map(0, readFile(file))
+    var levels: Levels = mutable.Map(0 -> readFile(file))
     var seen           = Set[String]()
     var minLevel       = 0
     var maxLevel       = 0
     do {
-      seen = seen + makeString(layout)
-      val (levels, minLevel, maxLevel) = updateLevels(layout, minLevel, maxLevel)
-      if (false) {
-        draw(layout)
-        Thread.sleep(100)
-      }
+      seen = seen + makeString(levels)
+      val (levels2, minLevel2, maxLevel2) = updateLevels(levels, minLevel, maxLevel)
+      levels = levels2
+      minLevel = minLevel2
+      maxLevel = maxLevel2
     } while (!seen.contains(makeString(levels)))
     countBugs(levels)
   }
 
-  def updateLevels(levels: Levels, minLevel: Int, maxLevel: Int): Levels = {
-//if (level >= minLevel && level <= maxLevel) {
-/// Array.fill(width)(Array.fill(width)(EMPTY))
-    (minLevel - 1).to(maxLevel + 1).foreach { level =>
-      0.until(width).foreach { y =>
-        0.until(width).foreach { x =>
-          val layout = levels(level)
-          val nbrAdjacent = nbrAdjacentBugs(levels, level, x, y)
-          if (layout(y)(x) == BUG) {
-            if (nbrAdjacent != 1) {
-              EMPTY
-            } else {
-              BUG
-            }
-          } else if (layout(y)(x) == EMPTY && nbrAdjacent == 1 || nbrAdjacent == 2) {
-            BUG
-          } else {
-            EMPTY
-          }
-        }
-      }
-
-    }
-    var y = -1
-    layout.map { line =>
-      y += 1
-      var x = -1
-      line.map { line =>
-        x += 1
-      }.toArray
-    }.toArray
+  def emptyLevel(): Layout = {
+    Array.fill(width)(Array.fill(width)(EMPTY))
   }
 
-  def nbrAdjacentBugs(levels: Levels, layout: Layout, x: Int, y: Int): Int = {
+  def updateLevels(levels: Levels, minLevel: Int, maxLevel: Int): (Levels, Int, Int) = {
+    var newMinLevel = Int.MinValue
+    var newMaxLevel = minLevel
+    val newLevels = mutable.Map[Int, Layout]()
 
+    (minLevel - 1).to(maxLevel + 1).map { level =>
+      var containsBugs = false
+      val layout       = levels(level)
+      val outerLevel = if (level == minLevel) {
+        emptyLevel()
+      } else {
+        levels(level - 1)
+      }
+      val innerLevel = if (level == maxLevel) {
+        emptyLevel()
+      } else {
+        levels(level + 1)
+      }
+      val newLevel = 0.until(width).map { y =>
+          0.until(width).map { x =>
+              val nbrAdjacent = nbrAdjacentBugs(layout, outerLevel, innerLevel, x, y)
+              if (layout(y)(x) == BUG) {
+                if (nbrAdjacent != 1) {
+                  EMPTY
+                } else {
+                  containsBugs = true
+                  BUG
+                }
+              } else if (layout(y)(x) == EMPTY && nbrAdjacent == 1 || nbrAdjacent == 2) {
+                containsBugs = true
+                BUG
+              } else {
+                EMPTY
+              }
+            }.toArray
+        }.toArray
+      if (containsBugs) {
+        if (minLevel == Long.MinValue) {
+          newMinLevel = level
+        }
+        newMaxLevel = level
+      }
+      if(level >= newMinLevel && level <= newMaxLevel){
+        newLevels(level) = newLevel
+      }
+    }
+    (newLevels, newMinLevel, newMaxLevel)
+  }
+
+  def countBug(layout: Layout, x: Int, y: Int): Int = {
+    if (layout(y)(x) == BUG) {
+      1
+    } else {
+      0
+    }
+  }
+
+  // Yikes!
+  def nbrAdjacentBugs(layout: Layout, outerLevel: Layout, innerLevel: Layout, x: Int, y: Int): Int = {
+    var sum = 0
+    if (x == 0) {
+      if (y == 0) {
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(outerLevel, 1, 2)
+        sum += countBug(outerLevel, 2, 1)
+      } else if (y >= 1 && y <= 3) {
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(outerLevel, 1, 2)
+      } else if (y == width - 1) {
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(outerLevel, 1, 2)
+        sum += countBug(outerLevel, 2, 3)
+      } else {
+        throw new RuntimeException("ERROR")
+      }
+    } else if (x == 1) {
+      if (y == 0) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(outerLevel, 2, 1)
+      } else if (y == 1 || y == 3) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(layout, x, y + 1)
+      } else if (y == 2) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(layout, x, y + 1)
+        0.until(width).foreach { y =>
+          sum += countBug(innerLevel, 0, y)
+        }
+      } else if (y == width - 1) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(outerLevel, 2, 3)
+      } else {
+        throw new RuntimeException("ERROR")
+      }
+    } else if (x == 2) {
+      if (y == 0) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(outerLevel, 2, 1)
+      } else if (y == 1) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y - 1)
+        0.until(width).foreach { x =>
+          sum += countBug(innerLevel, x, 0)
+        }
+      } else if (y == 2) {
+        // No-op
+      } else if (y == width - 2) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y + 1)
+        0.until(width).foreach { x =>
+          sum += countBug(innerLevel, x, 4)
+        }
+      } else if (y == width - 1) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(outerLevel, 2, 3)
+      } else {
+        throw new RuntimeException("ERROR")
+      }
+    } else if (x == width - 2) {
+      if (y == 0) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(outerLevel, 2, 1)
+      } else if (y == 1 || y == 3) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(layout, x, y + 1)
+      } else if (y == 2) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(layout, x, y + 1)
+        0.until(width).foreach { y =>
+          sum += countBug(innerLevel, width - 1, y)
+        }
+      } else if (y == width - 1) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x + 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(outerLevel, 2, 3)
+      } else {
+        throw new RuntimeException("ERROR")
+      }
+    } else if (x == width - 1) {
+      if (y == 0) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(outerLevel, 2, 1)
+        sum += countBug(outerLevel, 3, 2)
+      } else if (y == 1 && x <= 3) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x, y + 1)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(outerLevel, 3, 2)
+      } else if (y == width - 1) {
+        sum += countBug(layout, x - 1, y)
+        sum += countBug(layout, x, y - 1)
+        sum += countBug(outerLevel, 3, 2)
+        sum += countBug(outerLevel, 2, 3)
+      } else {
+        throw new RuntimeException("ERROR")
+      }
+    }
+    sum
   }
 
   def countBugs(levels: Levels): Int = {
-    -1
+    levels.map { entry =>
+      val layout = entry._2
+      layout.map { line =>
+        line.count(_ == BUG)
+      }.sum
+    }.sum
   }
 
   def makeString(levels: Levels): String = {
-
-    "TODO"
+    levels.map { entry =>
+        val layout = entry._2
+        layout
+          .map { line =>
+            line.mkString("")
+          }
+          .mkString("")
+      }
+      .mkString("")
   }
 
   def part1(file: String): BigInt = {
@@ -95,7 +257,7 @@ object Main extends App {
     var seen           = Set[String]()
     do {
       seen = seen + layout.map(_.mkString("")).mkString("")
-      layout = updateLayout(levels)
+      layout = updateLayout(layout)
     } while (!seen.contains(layout.map(_.mkString("")).mkString("")))
     calculateRating(layout)
   }
